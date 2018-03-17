2a3cb3f5a7f1dd7d3db2176841a4c9cba99eee26
kallelzied <kallelzied@live.com>
2018-03-17 16:03:20 +0100

    StripeDemoApplication

---
+++ src/main/java/com/stripe/StripeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripeDemoApplication.class, args);
+    }
+}
